e23a6f70ece67558d2c86cd7285cae6866d0180f
abdirahman <abdirahman.ismail@student.moringaschool.com>
2023-10-03 10:42:28 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1=31;
+let num2=2;
+const multiply =num1 * num2;
+
+
+let random=Math.floor(Math.random() * 10) +1;
+
+let num3= 9;
+let num4= 5;
+const mod= num3 % num4;
+
+const max=Math.max(6,7,8,9,14,16,20);
+
 
 // Write your code here
+
+
+
